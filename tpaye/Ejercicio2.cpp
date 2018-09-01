e36f38c8620375b796d125c1fe26c0c9db85ac6a
Franco Giannotti <francogiannotti115@gmail.com>
2018-09-01 01:38:30 -0300

    No solucione nada, pero por las dudas commiteo jajajaja

--- tpaye/Ejercicio2.cpp
+++ tpaye/Ejercicio2.cpp
@@ -35,7 +35,7 @@ int main() {
     /* initialize random seed: aca o en el void??*/
 //    std::time_t result = std::time(nullptr);
 //    srand (std::localtime(&result)->tm_sec);
-    srand(time(0));
+    srand(NULL);
     Pais Bolillero1[8] = {1, "Rusia", "UEFA", 1, "Alemania", "UEFA", 1, "Brasil", "Conmebol", 1, "Portugal", "UEFA", 1, "Argentina", "Conmebol", 1, "Belgica", "UEFA", 1, "Polonia", "UEFA", 1, "Francia", "UEFA"};
     Pais Bolillero2[8] = {2, " Espana", "UEFA", 2, "Peru", "Conmebol", 2, "Suiza", "UEFA", 2, "Inglaterra", "UEFA", 2, "Colombia", "Conmebol", 2, "Mexico", "Concacaf", 2, "Uruguay", "Conmebol", 2, "Croacia", "UEFA"};
     Pais Bolillero3[8] = {3, "Dinamarca", "UEFA", 3, "Islandia", "UEFA", 3, "Costa Rica", "Concacaf", 3, "Suecia", "UEFA", 3, "Tunez", "CAF", 3, "Egipto", "CAF", 3, "Senegal", "CAF", 3, "Iran", "AFC"};
@@ -48,18 +48,26 @@ int main() {
             Grupos[0].Equipo1 = Bolillero1[0];
         } else {
             Grupos[i].Equipo1 = Bolillero1[rand() % 8];
+
             while (fueSorteado(Grupos,i,Grupos[i].Equipo1)){
-                Grupos[i].Equipo1 = Bolillero1[rand() % 8];
+                    int a = rand() % 8;
+                while (fueSorteado(Grupos,i,Bolillero1[a])) {
+                    a = (a+1) % 8;
+                }
+                Grupos[i].Equipo1 = Bolillero1[a];
             }
         }
 
         Grupos[i].Equipo2 = Bolillero2[rand() % 8];
-        while ((Grupos[i].Equipo1.Conferedacion == "Conmebol" && Grupos[i].Equipo2.Conferedacion == "Conmebol") || fueSorteado(Grupos,i,Grupos[i].Equipo2)) {
+        while ((Grupos[i].Equipo1.Conferedacion == "Conmebol" && Grupos[i].Equipo2.Conferedacion == "Conmebol") || fueSorteado(Grupos,i,Grupos[i].Equipo2) || (Grupos[i].Equipo1.Conferedacion == Grupos[i].Equipo2.Conferedacion && Grupos[i].Equipo1.Conferedacion != "UEFA")) {
 
-            Grupos[i].Equipo2 = Bolillero2[rand() % 8];
-
+            int a = rand() % 8;
+            Grupos[i].Equipo2 = Bolillero2[a];
         }
+
+        //PASO 3
         Grupos[i].Equipo3 = Bolillero3[rand() % 8];
+        int c = 0;
         while ((Grupos[i].Equipo1.Conferedacion == "UEFA" && Grupos[i].Equipo2.Conferedacion == "UEFA" &&
                 Grupos[i].Equipo3.Conferedacion == "UEFA") ||
                (Grupos[i].Equipo1.Conferedacion == Grupos[i].Equipo3.Conferedacion &&
@@ -69,12 +77,12 @@ int main() {
             // a,b,c uefa, roll
             // c = a, a! = uefa roll
             // c = b & b != uefa roll
-
-
-            Grupos[i].Equipo3 = Bolillero3[rand() % 8];
+            int a = rand() % 8;
+            Grupos[i].Equipo3 = Bolillero3[a];
         }
-
+        // PASO 4
         Grupos[i].Equipo4 = Bolillero4[rand() % 8];
+        c = 0;
         while ((esUefa(Grupos[i]) && Grupos[i].Equipo4.Conferedacion == "UEFA") ||
                (Grupos[i].Equipo4.Conferedacion == Grupos[i].Equipo1.Conferedacion &&
                 Grupos[i].Equipo1.Conferedacion != "UEFA") ||
@@ -86,7 +94,8 @@ int main() {
             // d = a y a!=uefa
             // d = b y b!=uefa
             // d = c y c!=uefa
-            Grupos[i].Equipo4 = Bolillero4[rand() % 8];
+            int a = rand() % 8;
+            Grupos[i].Equipo4 = Bolillero4[a];
         }
     }
     for (int i = 0; i < 8; i++){
